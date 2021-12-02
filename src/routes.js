1b1134d617aec979ea5aeb8ef018ca30c1f38b94
unknown <sam.a.allegretto@gmail.com>
2021-12-01 18:55:32 -0700

    13.1 add page components, routing

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "./components/Home.vue";
+import Login from "./components/Login.vue";
+import Drivers from "./components/Drivers.vue";
+import NotFound from "./components/NotFound.vue";
+import ReviewDriver from "./components/ReviewDriver.vue";
+import RideInfo from "./components/RideInfo.vue";
+import Signup from "./components/Signup.vue";
+import Account from "./components/Account.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Home },
+    { path: "/account", component: Account },
+    { path: "/login", component: Login },
+    { path: "/drivers", component: Drivers },
+    {
+      path: "/drivers/:pk",
+      component: RideInfo,
+      children: [{ path: "review", component: ReviewDriver }],
+    },
+    { path: "/signup", component: Signup },
+    { path: "/:invalidroute(.*)", component: NotFound },
+  ],
+});
+
+export default router;
